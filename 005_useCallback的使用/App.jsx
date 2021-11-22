94417c78409799c50daab65eb559beb0bc95574c
dengwenjie <927561237@qq.com>
2021-11-22 19:12:41 +0800

    useCallback的使用

---
+++ 005_useCallback的使用/App.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+
+import Demo from './components/Demo';
+
+export default function App() {
+    const [flag, setFlag] = useState(true);
+
+    return (
+        <div>
+            <button
+                onClick={e => {
+                    setFlag(false);
+                }}
+            >
+                删除
+            </button>
+            {flag ? <Demo /> : ''}
+        </div>
+    );
+}
